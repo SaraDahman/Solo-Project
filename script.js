c30964baabbc4c205f739357b9b56c2fd899c98b
Sara Dahman <saritadh74@gmail.com>
2020-05-19 16:51:58 +0300

    base

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+$('#enter').click(function(){
+
+	if($('#age').val() === '' || $('#name').val() === ""){
+		alert ('please enter your info')
+	}
+	
+	else if( $('#age').val() < 18 ){
+	$('#info').remove()
+	var msg = $('<div></div>')			
+	msg.text("OH! you're a baby !!!!")
+	$('body').append(msg)
+	msg.css('font-size' , '129px')
+	
+	} else if( $('#age').val() > 18  ){
+	$('#info').remove()
+	var msg = $('<div></div>')			
+	msg.text("Hello Boomer")
+	$('body').append(msg)
+	msg.css('font-size' , '129px')
+}
+
+})
